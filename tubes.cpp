0d574cfa2bc55f2ac6c19a0ede0f26d079ee6b62
MohammadNizalMlna <mohammadnizalmaulana@gmail.com>
2024-05-27 19:48:22 +0700

    update tubes

--- tubes.cpp
+++ tubes.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <string>
 
 using namespace std;
@@ -36,16 +37,21 @@ class Bank{
         bool temukanAkun = false;
         for(const auto& acc : akun){
             if(acc.NomorRekening == norek){
-                cout << "Nomor Rekening: "<< acc.NomorRekening <<endl;
-                cout << "Username: " << acc.UserName<<endl;
-                cout << "Saldo: Rp" <<acc.saldo<<endl;
+                cout << left << setw(20) << "Nomor Rekening" 
+                    << left << setw(20) << "Username" 
+                    << left << setw(20) << "Saldo" << endl;
+                cout << left << setw(20) << acc.NomorRekening 
+                    << left << setw(20) << acc.UserName 
+                    << left << setw(20) << "Rp" + acc.saldo << endl;
                 temukanAkun = true;
                 break;
             }
         }
+        cout << endl;
         if (!temukanAkun){
             cout << "Tidak ada akun dengan nomor rekening tersebut"<<endl;
         }
+        cout << endl;
     }
 
     //Mengembalikan status apakahh ada akun yang sudah ditambahkan
@@ -90,9 +96,48 @@ class Bank{
             }else{
                 cout << "Saldo akun anda tidak mencukupi"<<endl;
             }
+            cout << endl;
+            return;
         }else{
             cout << "Nomor rekening yang anda masukan dan tuju tidak valid" <<endl;
         }
+        cout << endl;
+    }
+
+    //Menarik uang dari akun bank
+    void tarikUang(int norek, int jumlah){
+        for(auto& acc : akun){
+            if(acc.NomorRekening == norek){
+                int saldoAkun = stoi(acc.saldo);
+                if(saldoAkun >= jumlah){
+                    saldoAkun -= jumlah;
+                    acc.saldo = to_string(saldoAkun);
+                    cout << "Penarikan sukses" << endl;
+                }else{
+                    cout << "Saldo akun tidak mencukupi " <<endl;
+                }
+                cout << endl;
+                return;
+            }
+        }
+        cout << "Nomor rekening tidak ditemukan" << endl;
+        cout << endl;
+    }
+
+    //Menabung uang ke akun
+    void menabung(int norek, int jumlah){
+        for (auto& acc : akun){
+            if(acc.NomorRekening == norek){
+                int saldoAkun = stoi(acc.saldo);
+                saldoAkun += jumlah;
+                acc.saldo = to_string(saldoAkun);
+                cout << "Menabung sukses" <<endl;
+                cout << endl;
+                return;
+            }
+        }
+        cout <<"Nomor rekening tidak ditemukan " <<endl;
+        cout << endl;
     }
 };
 
@@ -104,12 +149,17 @@ int main(){
     string jumlahSaldo;
 
     menu:
-    cout << "Aplikasi Transfer Bank" <<endl;
-    cout << "1. Tambah Akun Bank" <<endl;
-    cout << "2. Munculkan Informasi akun" <<endl;
-    cout << "3. Ubah nomor rekening" <<endl;
-    cout << "4. Transfer saldo "<<endl;
-    cout << "5. Keluar" <<endl;
+    cout << "===================================="<<endl;
+    cout << "|       Aplikasi Transfer Bank     |" <<endl;
+    cout << "====================================" <<endl;
+    cout << "| 1. Tambah Akun Bank              |" <<endl;
+    cout << "| 2. Munculkan Informasi akun      |" <<endl;
+    cout << "| 3. Ubah nomor rekening           |"<<endl;
+    cout << "| 4. Transfer saldo                |"<<endl;
+    cout << "| 5. Tarik uang                    |" <<endl;
+    cout << "| 6. Menabung                      |" <<endl;
+    cout << "| 7. Keluar                        |" <<endl;
+    cout << "===================================="<<endl;
     cout << "Pilih operasi: ";
     cin >> pilih;
 
@@ -123,11 +173,13 @@ int main(){
             cin >> saldo;
             bank.tambahAkun(NomorRekening,UserName,saldo);
             cout << "Akun Berhasil ditambahkan"<<endl;
+            cout << endl;
             goto menu;
             break;
         case 2:
             if (!bank.akunSudahDitambahkan()){
                 cout << "Silahkan tambah akun terlebih dahulu" <<endl;
+                cout << endl;
                 goto menu;
             }
             cout << "Masukkan nomor rekening akun yang dicari: "; cin >> NomorRekening;
@@ -137,6 +189,7 @@ int main(){
         case 3:
             if (!bank.akunSudahDitambahkan()){
                 cout << "Silahkan tambahkan akun terlebih dahulu"<<endl;
+                cout << endl;
                 goto menu;
             }
             cout << "Masukkan nomor rekening yang ingin diubah: "; cin >>NomorRekening;
@@ -148,6 +201,7 @@ int main(){
         case 4 :
             if (!bank.akunSudahDitambahkan()){
                 cout << "Silahkan tambahkan akun terlebih dahulu" <<endl;
+                cout << endl;
                 goto menu;
             }
             cout << "Masukkan nomor rekening asal: "; cin >> norekAsal;
@@ -157,6 +211,32 @@ int main(){
             goto menu;
             break;
         case 5 :
+            if(!bank.akunSudahDitambahkan()){
+                cout << "Silahkan tambahkan akun terlebih dahulu" <<endl;
+                cout << endl;
+                goto menu;
+            }
+            cout << "Masukkan nomor rekening: ";
+            cin >> NomorRekening;
+            cout << "Masukkan Jumlah uang yang ingin ditarik: ";
+            cin >> jumlah;
+            bank.tarikUang(NomorRekening,jumlah);
+            goto menu;
+            break;
+        case 6 :
+            if(!bank.akunSudahDitambahkan()){
+                cout << "Silahkan tambahkan akun terlebih dahulu "<<endl;
+                cout << endl;
+                goto menu;
+            }
+            cout << "Masukkan nomor rekening: ";
+            cin >> NomorRekening;
+            cout << "Masukkan jumlah uang yang ingin ditabung: ";
+            cin >> jumlah;
+            bank.menabung(NomorRekening,jumlah);
+            goto menu;
+            break;
+        case 7 :
             cout << "Keluar dari program" <<endl;
             break;
         default:
